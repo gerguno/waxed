7edcb0622c2a8973168899eaf3800af8395bacb5
Oles Gergun <gerguno@gmail.com>
2016-05-03 23:42:20 +0300

    index page done

--- js/design.js
+++ js/design.js
@@ -1,1 +1,16 @@
 // http://www.w3schools.com/howto/howto_js_dropdown.asp
+
+$('.dropdown').click(function() {
+	$(this).next().css('top', ($(this).offset().top - 33));
+	$('.dropdown').not(this).next().fadeOut();
+	$(this).next().fadeToggle();
+});
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdown')) {
+  		$('.dropdown-content').fadeOut();
+    }
+}
+
+
